7fcd8208236995ca5466575dfa0f9309d955d37e
mfrank <markus.frank@informatik.tu-chemnitz.de>
2018-11-21 12:05:24 +0100

    added part A

---
+++ LoopingPaule.java
@@ -0,0 +1,31 @@
+import de.unistuttgart.iste.rss.oo.hamstersimulator.external.model.Hamster;
+import de.unistuttgart.iste.rss.oo.hamstersimulator.datatypes.Direction;
+import de.unistuttgart.iste.rss.oo.hamstersimulator.datatypes.Location;
+import de.unistuttgart.iste.rss.oo.hamstersimulator.external.model.Territory;
+
+/**
+ * Beschreiben Sie hier die Klasse LoopingPaule.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class LoopingPaule extends HamsterGame{
+
+    LoopingPaule(){        
+        super("/territories/territory-ub6.ter");        
+    }
+    
+    
+    void multiMove (int numberOfSteps) {
+
+    }
+    
+    void spreadGrainsInCurrentDirection() {
+
+    }
+    
+    void moveToNextWall() {
+
+    }
+    
+}
